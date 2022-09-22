6d07d519f72a852ef93733ad2edeaca3aa5e9899
Rui <rui0110@bu.edu>
2022-09-22 16:10:45 -0400

    try to finish read.py

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,20 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    m = []
+    with open(csv_file_path, 'r') as read_j:
+        csv_reader = csv.reader(read_j)
+        for row in csv_reader:
+            r = row
+            for i in range(0,len(r)):
+                x = r[i]
+                if ord(x[0]) in range(49,58):
+                    x = int(x)
+                    r[i] = x
+            m.append(r)
+        return m
+    
